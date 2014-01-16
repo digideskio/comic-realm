ce038f52b1e0e42c2b03ae168e62e3cf860eed0b
James Ebentier <jebentier@gmail.com>
2014-01-16 13:21:25 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ComicRealm::Application.config.secret_key_base = '53d0805ed0724f3d70010848c653530727e66f41ed9d64d9cebcd36fa652031355e3b946dd64d6a8501acfcf4d07f4f52a8f03789e65a5e16c50c0cadf582cef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ComicRealm::Application.config.session_store :cookie_store, key: '_comic-realm_session'
