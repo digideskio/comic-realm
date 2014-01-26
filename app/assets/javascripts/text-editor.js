f216f254648b0711f768e668f2cf4de239ce297f
James Ebentier <jebentier@gmail.com>
2014-01-25 20:12:51 -0800

    working on the text editor

--- app/assets/javascripts/text-editor.js
+++ app/assets/javascripts/text-editor.js
@@ -1,7 +1,14 @@
 $(function () {
+	function handel_overflow(e) {
+		console.log('overflowing!');
+	}
+
 	$('.toolbar .btn').tooltip({'placement': 'bottom'});
-	var $editor = $('#editor').wysiwyg({
+	
+	var editor = $('#editor').wysiwyg({
 		dragAndDropImages: false,
 		activeToolbarClass: 'btn-pressed'
 	});
+
+	editor.get(0).addEventListener('overflowchanged', handel_overflow, false);
 });
